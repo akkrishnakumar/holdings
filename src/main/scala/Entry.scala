029f282e45813d000c1de34b0896b91c7a0a4795
Akhil Krishnakumar <akhil.krishnakumar.iicmr@gmail.com>
2023-12-02 16:09:25 +0100

    Add fake call which uses a simple Java http request client.

--- src/main/scala/Entry.scala
+++ src/main/scala/Entry.scala
@@ -1,42 +1,52 @@
 import Operations.{Consolidate, ScreenerToTV, TVImport}
 
 import java.io.File
+import java.net.{HttpURLConnection, URL}
 import scala.io.Source
 import scala.quoted.*
 
-object Entry extends App:
+@main
+def Entry(command: String, fileName: String) =
   println("=" * 100)
-  performOperation(ScreenerToTV)
+  performOperation(command, fileName)
   println("=" * 100)
 
-def performOperation(operation: Operations) =
-  operation match
-    case Consolidate  => consolidateHoldings()
-    case TVImport     => tvImport()
-    case ScreenerToTV => screenerToTv()
+def performOperation(command: String, fileName: String) =
+  command match
+    case "Consolidate"  => consolidateHoldings(fileName)
+    case "TVImport"     => tvImport(fileName)
+    case "ScreenerToTV" => screenerToTv(fileName)
+    case "fakeCall"     => fakeCall
 
-def consolidateHoldings() =
+def consolidateHoldings(fileName: String) =
   println("Reading File....")
-  val filePath = "/Users/akhil/Downloads/holdings.csv"
+  val filePath = s"/Users/akhil/Downloads/$fileName.csv"
   val holdings = csvToStrings(new File(filePath)).asHoldings
   consolidate(holdings)
 
-def tvImport() =
+def tvImport(fileName: String) =
   println("Importing for Trading View...")
-  val filePath = "/Users/akhil/Downloads/Trend Template.csv"
-  tvFileImport("Trend Template.txt")(
-    csvToStrings(new File(filePath)).asTVTickers()
-  )
+  val filePath = s"/Users/akhil/Downloads/$fileName.csv"
+  tvFileImport(s"$fileName.txt")(csvToStrings(new File(filePath)).asTVTickers())
   println("Done!!")
 
 /** JS Script Get href and stock name
   * [...document.querySelectorAll("td>a")].map(e => e.href + ", " +
-  * e.innerText).join("\n") Href has the NSE Ticker name. Compress stock name if
-  * the Ticker name is not NSE compatible.
+  * e.innerText).join(":::") Href has the NSE Ticker name. Compress stock name
+  * if the Ticker name is not NSE compatible.
   */
-def screenerToTv() =
+def screenerToTv(fileName: String) =
   println("Importing from Screener to TradingView List...")
-  val filePath = "/Users/akhil/Downloads/screener.txt"
+  val filePath = s"/Users/akhil/Downloads/$fileName.txt"
   val rows = safeRead(filePath)(lines => lines.head.split(":::").toList)
-  tvFileImport("CANSLIM.txt")(rows.asScreenerTickers)
+  tvFileImport(s"$fileName-TVImport.txt")(rows.asScreenerTickers)
   println("Done!!")
+
+def fakeCall =
+
+  val url = new URL("https://www.google.com")
+  val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+  connection.setRequestMethod("GET")
+
+  val responseCode = connection.getResponseCode
+  println(s"Response Code from Google is $responseCode")
